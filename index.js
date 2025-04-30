ea3e23c86e6d77a2817253bd8596aca49b49e53e
tarantanya <tarantanya@gmail.com>
2025-04-30 11:46:21 +0000

    init

--- index.js
+++ index.js
@@ -34,8 +34,10 @@ class Instance {
         this._count = await this._db.getCountCards();
       }
       const cards = await this._db.getCards(this._count);
+      console.log(cards);
       for (const card of cards) {
         const msg = this._telegram.cardFormatMsg(card);
+        console.log(msg);
         await this._telegram.sendTelegramMessage(msg);
         await this._db.updCards(card);
       }
